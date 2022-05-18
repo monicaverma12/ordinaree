b0d875f443bfb289e30b3cce9aabcbe90dc2cc77
monica verma <81722074+monicaverma12@users.noreply.github.com>
2022-05-18 11:32:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import joblib
+
+# Title
+st.header("Ordinaree Machine Learning Web Page")
+
+# Input bar 1
+Sale = st.number_input("Enter Expected Sales")
+
+# If button is pressed
+if st.button("Click to get result"):
+    # Unpickle classifier
+    clf = joblib.load("No_of_Orders_predictor.pkl")
+
+    # Get prediction
+    prediction = clf.predict([Sale])[0]
+
+    # Output prediction
+    st.text(f"Total no of orders woould be : {prediction}")
